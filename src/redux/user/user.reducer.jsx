d6a550cf98cb48562a12288a168df86312679200
J Shan <jshan@js-imac.gateway.sonic.net>
2020-02-14 19:30:26 -0800

    updating our app to redirect to home if user logged in

--- src/redux/user/user.reducer.jsx
+++ src/redux/user/user.reducer.jsx
@@ -1,3 +1,5 @@
+import { UserActionTypes } from './user.types';
+
 //reducers are functions that accepts two properties
 // a state and an action. action are objects that have types and payload
 const INITIAL_STATE = {
@@ -6,7 +8,7 @@ const INITIAL_STATE = {
 
 const userReducer = (state = INITIAL_STATE, action) => {
     switch(action.type){
-        case 'SET_CURRENT_USER':
+        case UserActionTypes.SET_CURRENT_USER:
             return {
                 ...state, 
                 currentUser: action.payload
